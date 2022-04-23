f176eb87136dff762d01a56547dafba3bf3bf60f
Luiz Anselmo <lavra2021@gmail.com>
2022-04-23 13:48:35 -0300

    Upgrade laravel 8

--- database/seeders/ConfigSiteTableSeeder.php
+++ database/seeders/ConfigSiteTableSeeder.php
@@ -1,12 +1,14 @@
 <?php
 
+namespace Database\Seeders;
+
 use Illuminate\Database\Seeder;
 use App\Models\ConfigSite;
 
 class ConfigSiteTableSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Run the database seeders.
      *
      * @return void
      */
